93614f07abbbe789d66a0c040ed703ce0038a12d
Yasuosword <1677280477@qq.com>
2020-12-27 19:54:47 +0800

    kklk

--- demo/api/client-query.php
+++ demo/api/client-query.php
@@ -11,18 +11,42 @@ if (!$con)
 //mysqli_select_db($con,"test");
 mysqli_set_charset($con, "utf8");
 
-if(!isset($_GET['type'])) {
+if(!isset($_GET['type'])||!isset($_GET['word'])) {
     $sql = "select * from client";
-}
-if(isset($_GET['type'])&&$_GET['type']==0)
-{$ID=$_GET['word'];   $sql="select *from client where CusID=$ID";}
-if(isset($_GET['type'])&&$_GET['type']==1)
-{$name=$_GET['word'];   $sql="select *from client where CusName='$name'";}
+    $result = mysqli_query($con,$sql);
+    $count=mysqli_num_rows($result);
+    $arr = array();
+// 输出每行数据
+    while($row = $result->fetch_assoc()) {
+        $rongyu=count($row);//不能在循环语句中，由于每次删除row数组长度都减小
+        for($i=0;$i<$rongyu;$i++){
+            unset($row[$i]);//删除冗余数据
+        }
+        array_push($arr,$row);
 
+    }
+    $output = array(
+        'code' => 0,
+        'msg' =>'',
+        'count' => $count,
+        'data' => $arr
+    );
+}
+if(isset($_GET['type'])==null||isset($_GET['word'])==null)
+{$output = array(
+    'code' => 0,
+    'msg' =>'',
+    'count' => 0,
+    'data' =>''
+);}
+if(isset($_GET['type'])&&isset($_GET['word'])){
+if($_GET['type']==0)
+{$ID=$_GET['word'];   $sql="select *from client where CusID=$ID";}
+if($_GET['type']==1)
+{$name=$_GET['word'];   $sql="select *from client where CusName='$name'";}
 
 $result = mysqli_query($con,$sql);
 $count=mysqli_num_rows($result);
-
 $arr = array();
 // 输出每行数据
 while($row = $result->fetch_assoc()) {
@@ -33,13 +57,14 @@ while($row = $result->fetch_assoc()) {
     array_push($arr,$row);
 
 }
-
 $output = array(
     'code' => 0,
     'msg' =>'',
     'count' => $count,
     'data' => $arr
 );
+}
+
 //print_r($arr);
 echo json_encode($output, JSON_UNESCAPED_UNICODE);//json编码
 
